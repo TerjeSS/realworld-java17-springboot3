c169d6542ce0b1fd648cde8ebc044412eb6c9ec6
Jehan <jehan.khodabocus@accenture.com>
2023-09-08 13:58:23 +0200

    Processing orders

--- src/main/java/io/shirohoo/realworld/domain/order/Orders.java
+++ src/main/java/io/shirohoo/realworld/domain/order/Orders.java
@@ -66,7 +66,7 @@ public class Orders {
         this.snailMailAddress = snailMailAddress;
     }
 
-        public void addOrderArticle(OrderArticle orderArticle) {
+    public void addOrderArticle(OrderArticle orderArticle) {
         this.orderArticles.add(orderArticle);
 
     }
@@ -77,7 +77,6 @@ public class Orders {
     public void setProcessed(boolean processed) {
         this.processed = processed;
     }
-
 
     public void setErrorType(String errorType) {
         this.errorType = errorType;
