5a605894cf6710257c7f8f721ae0309bfe295da5
Terje <testTerje@gmail.com>
2023-09-13 15:43:38 +0200

    added a RatingService

--- src/main/java/io/shirohoo/realworld/domain/rating/RatingRepository.java
+++ src/main/java/io/shirohoo/realworld/domain/rating/RatingRepository.java
@@ -2,5 +2,9 @@ package io.shirohoo.realworld.domain.rating;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface RatingRepository extends CrudRepository<Rating, Integer> {
+
+    List<Rating> findAllByArticleId(Integer id);
 }
